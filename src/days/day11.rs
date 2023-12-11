f807ad3ef50bc93cf0758750e651e6da110e79c2
Rik van Toor <rik.van.toor@gmail.com>
2023-12-11 07:02:00 +0100

    Day 11

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,25 +1,100 @@
+use nom::branch::alt;
+use nom::character::complete::{char, newline};
+use nom::combinator::map as pmap;
+use nom::multi::{many1, separated_list0};
 use nom::IResult;
+use std::collections::HashSet;
 
 use crate::days::Day;
 
 pub struct Day11;
 
+#[derive(Debug, PartialEq)]
+pub enum Pixel {
+  Galaxy,
+  Empty,
+}
+
+fn parse_pixel(input: &str) -> IResult<&str, Pixel> {
+  alt((
+    pmap(char('#'), |_| Pixel::Galaxy),
+    pmap(char('.'), |_| Pixel::Empty),
+  ))(input)
+}
+
+fn solve(steps: usize, input: &[Vec<Pixel>]) -> usize {
+  let mut galaxies: HashSet<(usize, usize)> = input
+    .iter()
+    .enumerate()
+    .flat_map(|(y, row)| {
+      row.iter()
+        .enumerate()
+        .filter_map(|(x, p)| match *p {
+          Pixel::Galaxy => Some((x, y)),
+          _ => None,
+        })
+        .collect::<Vec<(usize, usize)>>()
+    })
+    .collect();
+  let mut y = 0;
+  let mut max_y = input.len() - 1;
+  while y <= max_y {
+    if galaxies.iter().filter(|(_, gy)| *gy == y).count() == 0 {
+      galaxies = galaxies
+        .iter()
+        .map(|(gx, gy)| (*gx, if *gy > y { gy + steps - 1 } else { *gy }))
+        .collect();
+      max_y += steps - 1;
+      y += steps;
+    } else {
+      y += 1;
+    }
+  }
+  let mut x = 0;
+  let mut max_x = input[0].len() - 1;
+  while x <= max_x {
+    if galaxies.iter().filter(|(gx, _)| *gx == x).count() == 0 {
+      galaxies = galaxies
+        .iter()
+        .map(|(gx, gy)| (if *gx > x { gx + steps - 1 } else { *gx }, *gy))
+        .collect();
+      max_x += steps - 1;
+      x += steps;
+    } else {
+      x += 1;
+    }
+  }
+
+  galaxies
+    .iter()
+    .enumerate()
+    .map(|(i, (gx, gy))| {
+      galaxies
+        .clone()
+        .iter()
+        .skip(i + 1)
+        .map(|(gx2, gy2)| gx.abs_diff(*gx2) + gy.abs_diff(*gy2))
+        .sum::<usize>()
+    })
+    .sum()
+}
+
 impl Day for Day11 {
-  type Input = String;
+  type Input = Vec<Vec<Pixel>>;
 
-  fn parse(_input: &str) -> IResult<&str, Self::Input> {
-    unimplemented!("parser")
+  fn parse(input: &str) -> IResult<&str, Self::Input> {
+    separated_list0(newline, many1(parse_pixel))(input)
   }
 
-  type Output1 = String;
+  type Output1 = usize;
 
-  fn part_1(_input: &Self::Input) -> Self::Output1 {
-    unimplemented!("part_1")
+  fn part_1(input: &Self::Input) -> Self::Output1 {
+    solve(2, input)
   }
 
-  type Output2 = String;
+  type Output2 = usize;
 
-  fn part_2(_input: &Self::Input) -> Self::Output2 {
-    unimplemented!("part_2")
+  fn part_2(input: &Self::Input) -> Self::Output2 {
+    solve(1000000, input)
   }
 }
